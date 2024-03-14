76141e06e4e6c960ec02d89719b099e7121c48cc
Ethan Kyle <afrikapetersek@gmail.com>
2024-03-14 20:08:58 +0200

    code

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
-function validateSyntax() {
-    let input = document.getElementById('petInput').value;
-    // Validation logic goes here
-    let result = ''; // Placeholder for validation result
 
-    // TODO: Write your validation logic here
-        // Check if input starts with 'pet_' and followed by alphanumeric characters
-
-            document.getElementById('result').innerText = result;
-}
-
+function validateSyntax() { let input = document.getElementById('petInput').value; 
+// Validation logic goes here 
+   let result = ''; 
+   // Placeholder for validation result
+    // TODO: Write your validation logic here 
+    // Check if input starts with 'pet_' and followed by alphanumeric characters 
+    if(/pet_[a-zA-Z0-9]+$/.test(input)) { result= 'valid Synatx'; 
+   } else{ result='Invalid Synatx';
+    } document.getElementById('result').innerText = result; }
 
